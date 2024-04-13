a7c4208c5f4556d1073cba67e8accf5a36e14d92
David Singleton <davidjsingleton18@gmail.com>
2024-04-13 16:28:06 -0700

    Add selectors for player names and player display
    Add function to control player name display and update based on if player names changed or were left empty

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -152,7 +152,7 @@ function TicTacToe() {
         console.log(`${winningPlayer} is the winner!`);
         return true;
       }
-      if (tiedGame === gameBoard.size * gameBoard.size) {
+      if (tiedGame === gameBoard.size * gameBoard.size - 1) {
         console.log("It's a tied game!");
         return true;
       }
@@ -171,37 +171,67 @@ function TicTacToe() {
 
 // Function for playing Tic Tac Toe
 function PlayTicTacToe() {
+  // Initializes the tic tac toe board
   const board = TicTacToe();
 
+  // These values speak for themself except "players", which is used to keep track of player names and shapes
   let currentBoardSize = board.getCurrentBoardSize();
   let roundsPlayed = 0;
   let victor = false;
 
-  let playerOneName = 'Player one';
-  let playerTwoName = 'Player two';
+  let playerOneName = 'Player One';
+  let playerTwoName = 'Player Two';
+  let players;
 
-  let playerOneNameInput = document.querySelector(
-    '[data-player-one-name]'
-  ).value;
-  let playerTwoNameInput = document.querySelector(
-    '[data-player-two-name]'
-  ).value;
+  // Used to get the input values and display them to the user in the DOM
+  let playerOneNameInput = document.querySelector('[data-player-one-name]');
+  let playerTwoNameInput = document.querySelector('[data-player-two-name]');
 
+  let playerOneNameDisplay = document.querySelector(
+    '[data-player-one-name-display]'
+  );
+  let playerTwoNameDisplay = document.querySelector(
+    '[data-player-two-name-display]'
+  );
+
+  // Used to handle naming the player, displaying it in the DOM or using the default names if the player cancels the initial modal
   function playerNames() {
-    if (playerOneNameInput === '') {
-      playerOneName = 'Player One';
+    // Used to handle and update player names
+    function updatePlayerNames(tttNameInput, tttName, tttNameDisplay) {
+      if (tttNameInput.value === '') {
+        tttNameDisplay.textContent = tttName;
+        return tttName;
+      }
+      tttName = tttNameInput.value;
+      tttNameDisplay.textContent = tttNameInput.value;
+      return tttNameInput.value;
     }
+
+    playerOneName = updatePlayerNames(
+      playerOneNameInput,
+      playerOneName,
+      playerOneNameDisplay
+    );
+    playerTwoName = updatePlayerNames(
+      playerTwoNameInput,
+      playerTwoName,
+      playerTwoNameDisplay
+    );
+
+    players = [
+      { name: playerOneName, shape: 'X' },
+      { name: playerTwoName, shape: 'O' },
+    ];
   }
 
-  let players = [
-    { name: playerOneName, shape: 'X' },
-    { name: playerTwoName, shape: 'O' },
-  ];
+  playerNames();
 
+  // Sets the current player as Player 1 always
   let currentPlayer = players[0];
 
   console.log(`It's ${currentPlayer.name}'s turn!`);
 
+  // Updates current player by seeing who current player is and switching when that player makes a move
   function getCurrentPlayer() {
     currentPlayer =
       currentPlayer.name === players[0].name ? players[1] : players[0];
@@ -211,6 +241,7 @@ function PlayTicTacToe() {
     console.log(`It's currently ${currentPlayer.name}'s turn!`);
   }
 
+  // Sets the event listeners on each square regardless of board size on each new game/reset game
   function playerClickedSquare() {
     const squares = document.querySelectorAll('[data-index]');
 
@@ -223,29 +254,25 @@ function PlayTicTacToe() {
 
   playerClickedSquare();
 
+  // Handles playing of each round, checking to make sure moves are valid and placement is valid
   function playRound(placement, currentSquare) {
-    if (
-      roundsPlayed !== currentBoardSize * currentBoardSize &&
-      victor !== true
-    ) {
+    const boardSize = currentBoardSize * currentBoardSize;
+
+    if (roundsPlayed !== boardSize && victor !== true) {
       const placedMarker = board.playerMove(
         placement,
         currentPlayer.shape,
         currentSquare
       );
 
-      if (
-        placedMarker &&
-        placement >= 0 &&
-        placement <= currentBoardSize * currentBoardSize - 1
-      ) {
+      if (placedMarker && placement >= 0 && placement <= boardSize - 1) {
         console.log(
           `${currentPlayer.name} places their marker ${currentPlayer.shape} at ${placement}`
         );
 
         victor = board.checkWinner(currentPlayer.name, roundsPlayed);
 
-        if (victor !== true) {
+        if (roundsPlayed !== boardSize && victor !== true) {
           roundsPlayed += 1;
           getCurrentPlayer();
           console.log(`It's ${currentPlayer.name}'s turn now!`);
@@ -261,6 +288,7 @@ function PlayTicTacToe() {
     board.createNewBoard();
     currentBoardSize = board.getCurrentBoardSize();
     playerClickedSquare();
+    playerNames();
     currentPlayer = players[0];
     roundsPlayed = 0;
     victor = false;
@@ -268,7 +296,7 @@ function PlayTicTacToe() {
     console.log(`It's ${currentPlayer.name}'s turn!`);
   }
 
-  // DOM elements used to start a new game and adjust the game board size
+  // DOM elements used to start a new game and adjust the game board size using user input and buttons
   const newGame = document.querySelector('[data-new-game]');
   const newGameOptions = document.querySelector('[data-new-game-options]');
   const startNewGame = document.querySelector('[data-start-new-game]');
@@ -299,9 +327,9 @@ function PlayTicTacToe() {
     beginNewGame();
   });
 
-  // window.onload = newGameOptions.showModal();
+  window.onload = newGameOptions.showModal();
 
-  return { playRound, showCurrentPlayer, newGame };
+  return { playRound, showCurrentPlayer, beginNewGame };
 }
 
 const game = PlayTicTacToe();
